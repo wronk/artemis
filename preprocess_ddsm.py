e7e07ceac0132d95a63e59a529f9f33af247ae90
wronk <wronk.mark@gmail.com>
2016-10-18 18:07:49 -0700

    Functional preprocessing code

--- preprocess_ddsm.py
+++ preprocess_ddsm.py
@@ -1,5 +1,5 @@
 """
-preprocess_bc_test.py
+preprocess_ddsm.py
 
 @author: wronk
 
@@ -8,8 +8,7 @@ Preprocess (resize, crop, normalize) BC test data and save
 
 import os
 from os import path as op
-#import cv2
-
+import cv2
 
 ###################
 # Set globals
@@ -17,37 +16,62 @@ from os import path as op
 save_data = True
 fold_save = 'preprocessed'
 
-data_dir = './test_data/'
+data_dir = '/media/Toshiba/Code/ddsm_data/'
+save_dir = '/media/Toshiba/Code/ddsm_data_preproc/'
 base_str_classes = ['normal', 'benign', 'cancer']
-case_base_str = 'case_'
-base_str_img = ['*LCC.png', '*LMLO.png',
-                '*RCC.png', '*RMLO.png']
-new_img_size = [400, 300]
+n_base_classes = [12, 14, 15]
+#base_str_classes = ['normal']
+#n_base_classes = [1]
+case_base_str = 'case'
+base_str_img = ['LEFT_CC.png', 'LEFT_MLO.png',
+                'RIGHT_CC.png', 'RIGHT_MLO.png']
+new_img_size = (400, 800)
+
+
+def check_dir(path):
+    if not op.isdir(path):
+        os.mkdir(path)
 
 ####################
 # Preprocess images
 ####################
+#TODO: Once working, convert to functions/reduce for loops
 
-for class_fold in base_str_classes:
-    batch_dir = op.join(data_dir, class_fold)
+for class_fold, n_base_class in zip(base_str_classes, n_base_classes):
+    batch_folds = [class_fold + '_%02i' % batch_num
+                   for batch_num in range(1, n_base_class + 1)]
 
-    # Get inidividual case directory names
-    case_list = [c for c in os.listdir(batch_dir)
-                 if op.isdir(c) and case_base_str in c]
+    for batch_fold in batch_folds:
+        # Get inidividual case directory names
+        batch_dir = op.join(data_dir, batch_fold)
+        case_list = [c for c in os.listdir(batch_dir)
+                     if op.isdir(op.join(batch_dir, c)) and case_base_str in c]
 
-    # Loop through each individual case
-    # TODO: change to use base_str_img
-    for case in case_list:
-        img_list = [img for img in op.isdir(case)
-                    if '.JPG' in img]
+        check_dir(op.join(save_dir, batch_fold))
 
-        for fname_img in img_list:
-            img = cv2.imread(fname_img)
-            img_resized = cv2.resize(img, None, new_img_size[0], new_img_size[1],
-                                     interpolation='INTER_AREA')
+        # Loop through each individual case
+        # TODO: change to use base_str_img
+        for ci, case in enumerate(case_list):
+            print 'Processing case %i/%i, %s' % (ci + 1, len(case_list), case)
+            case_dir = op.join(batch_dir, case)
+            img_dir = os.listdir(op.join(batch_dir, case, 'PNGFiles'))
+            img_list = [img for img in img_dir if '.png' in img]
 
-            if save_data:
-                fname, fext = op.splitext(fname_img.split)
-                fname_save = fname + '_preproc' + fext
+            check_dir(op.join(save_dir, batch_fold, case))
 
-                cv2.imwrite(fname_save, img_resize)
+            # Load and resize each image
+            for filepath_img in img_list:
+                filepath_img = op.join(case_dir, 'PNGFiles', filepath_img)
+                img = cv2.imread(filepath_img, 0)
+                img_resized = cv2.resize(img, new_img_size,
+                                         interpolation=cv2.INTER_AREA)
+
+                img_file_name, ext = op.splitext(op.basename(filepath_img))
+                fname_save = op.join(save_dir, batch_fold, case,
+                                     img_file_name + '_preproc' + ext)
+
+                if save_data:
+
+                    cv2.imwrite(fname_save, img_resized)
+                else:
+                    print 'Would have saved to: ' + fname_save
